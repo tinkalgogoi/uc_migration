a667d37723e72f6449d6a629a998278e66215a5d
tinkalgogoi <tinkalgogoi@gmail.com>
2017-02-13 15:45:54 +0530

    using our own http handler

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	// 	w.Write([]byte("Hello World"))
+	// })
+
+	http.Handle("/", new(MyHandler))
 
 	http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+	http.Handler // we will implement this handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public" + req.URL.Path
+
+	// ReadFile will load all the resources in the path to the Memory, so better to use bufferedReader
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+
 }
