08d318153abdc8a0364de5b5889e84c47d29efc7
tinkalgogoi <tinkalgogoi@gmail.com>
2017-02-13 15:18:32 +0530

    simple Hello world web app

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
