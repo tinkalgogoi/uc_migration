cd0bed498a3a35e7cd8da1502b664252031e0631
tinkalgogoi <tinkalgogoi@gmail.com>
2017-02-14 12:02:05 +0530

    Using go templates for dynamic data in html

--- main.go
+++ main.go
@@ -1,13 +1,86 @@
 package main
 
-import "net/http"
+import (
+	"bufio"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
 
-	// built in server to handle request
-	http.ListenAndServe(":9000", http.FileServer(http.Dir("public")))
+	// getting the pointer to templates files
+	templates := populateTemplates()
+
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, req *http.Request) {
+			requestedFile := req.URL.Path[1:]
+			// matching requested url with templates. Need to call the templates as ex. ip:port/index
+			template := templates.Lookup(requestedFile + ".html")
+
+			var context interface{} = nil
+
+			if template != nil {
+				// here we inject data to the template but right now its nil
+				template.Execute(w, context)
+			} else {
+				w.WriteHeader(404)
+			}
+		})
+
+	http.HandleFunc("/img/", serveResource)
+	http.HandleFunc("/css/", serveResource)
+	http.HandleFunc("/js/", serveResource)
+
+	http.ListenAndServe(":9000", nil)
 }
 
-type MyHandler struct {
-	http.Handler // we will implement this handler
+// serving static resource
+func serveResource(w http.ResponseWriter, req *http.Request) {
+	path := "public" + req.URL.Path
+	var contentType string
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path, ".js") {
+		contentType = "text/js"
+	} else if strings.HasSuffix(path, ".png") {
+		contentType = "image/png"
+	} else {
+		contentType = "text/plain"
+	}
+
+	f, err := os.Open(path)
+
+	if err == nil {
+		defer f.Close()
+		w.Header().Add("Content Type", contentType)
+
+		br := bufio.NewReader(f)
+		br.WriteTo(w)
+	} else {
+		w.WriteHeader(404)
+	}
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+
+	basePath := "templates"
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths,
+				basePath+"/"+pathInfo.Name())
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+
+	return result
 }
